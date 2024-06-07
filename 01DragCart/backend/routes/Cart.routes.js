21c50381499ec7d965dd733ecc609eb2732ebe76
Vishal Bodkhe <vishalbodkhe@gmail.com>
2024-06-07 16:22:10 +0530

    deleted backend from dragCart

--- 01DragCart/backend/routes/Cart.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createCart } from "../controllers/Cart.controllers.js";
-
-const router = express.Router();
-
-router.get("/cart-info", createCart);
-
-export default router;
